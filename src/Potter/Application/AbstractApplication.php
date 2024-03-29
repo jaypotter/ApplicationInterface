91d3181b8550c9c11d1743db0b575593a741d278
Jay Potter <j@ypotter.ca>
2024-03-29 10:59:19 -0400

    Create AbstractApplication.php

---
+++ src/Potter/Application/AbstractApplication.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Potter\Application;
+
+abstract class AbstractApplication implements ApplicationInterface
+{
+    abstract public function runningInConsole(): bool;
+}
