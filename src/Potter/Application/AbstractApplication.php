79cba9d1ce72f63ef362722d7f8b8caf64772e9d
Jay Potter <j@ypotter.ca>
2024-03-29 11:04:43 -0400

    session

--- src/Potter/Application/AbstractApplication.php
+++ src/Potter/Application/AbstractApplication.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace Potter\Application;
 
+use \Potter\Session\SessionInterface;
+
 abstract class AbstractApplication implements ApplicationInterface
 {
+    abstract public function getSession(): SessionInterface;
+    abstract protected function setSession(SessionInterface $session): void;
+    
     abstract public function runningInConsole(): bool;
 }
