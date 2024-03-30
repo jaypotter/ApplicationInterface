db3377f386d59865314a9e6451cd7a2dd4788a55
Jay Potter <j@ypotter.ca>
2024-03-29 21:27:45 -0400

    remove getSession

--- src/Potter/Application/AbstractApplication.php
+++ src/Potter/Application/AbstractApplication.php
@@ -4,10 +4,7 @@ declare(strict_types=1);
 
 namespace Potter\Application;
 
-use \Potter\Session\SessionInterface;
-
 abstract class AbstractApplication implements ApplicationInterface
 {
-    abstract public function getSession(): SessionInterface;
     abstract public function runningInConsole(): bool;
 }
--- src/Potter/Application/ApplicationInterface.php
+++ src/Potter/Application/ApplicationInterface.php
@@ -4,10 +4,7 @@ declare(strict_types=1);
 
 namespace Potter\Application;
 
-use \Potter\Session\SessionInterface;
-
 interface ApplicationInterface 
 {
-    public function getSession(): SessionInterface;
     public function runningInConsole(): bool;
 }
